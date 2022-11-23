ce26604a4a695f88e4d08304dd519ea0b80fc221
SeungMin <osminosmin7180@gmail.com>
2022-11-23 22:48:49 +0900

    register 거주지 추가

--- app/src/main/java/com/example/paperbox/fragment/refund.java
+++ app/src/main/java/com/example/paperbox/fragment/refund.java
@@ -1,66 +1,72 @@
 package com.example.paperbox.fragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.paperbox.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link refund#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class refund extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    ImageButton imageButton1;
+    ImageButton imageButton2;
+    ImageButton imageButton3;
+    ImageButton imageButton4;
+    ImageButton imageButton5;
+    ImageButton imageButton6;
+    ImageButton imageButton7;
+    ImageButton imageButton8;
+    ImageButton imageButton9;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public refund() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment refund.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static refund newInstance(String param1, String param2) {
-        refund fragment = new refund();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_refund, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_refund, container, false);
+
+
+
+        return v;
+    }
+
+    public void show() {
+        AlertDialog.Builder dig = new AlertDialog.Builder(getContext());
+
+        dig.setTitle("2300₩ 구매하기");
+        dig.setMessage("해당 강아지로 체험하시겠습니까?\n한 번 구매한 후에는 자동 소장됩니다.");
+        dig.setIcon(R.drawable.ic_launcher_foreground).setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getContext(),"현재는 구매하실 수 없습니다.",Toast.LENGTH_SHORT).show();
+
+                //displayToast("ok");
+            }
+        });//onClickListner : ok누르면 뭘할지,
+        dig.setNegativeButton("no", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getContext(),"취소 되었습니다.",Toast.LENGTH_SHORT).show();
+
+                //displayToast("no");
+            }
+        });//no버튼 누르면 뭐할지 _토스트를 하게 했다.//두가지 버전으로 가능.~.~; .~;
+        dig.show();
     }
 }
